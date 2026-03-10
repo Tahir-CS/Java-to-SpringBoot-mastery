d233c84edc9eab4b37605c7a86f445c7a6007e5c
Muhammad Tahir <2024cs704@student.uet.edu.pk>
2026-03-10 12:49:25 +0500

    implement Connectable interface to learn abstractio

---
+++ Milestone 1- OOP ARCHITECTURE/Connectable.java
@@ -0,0 +1,11 @@
+//Interface only defines what a function who implements this interface should do !
+//this interface connectable says the class who implements this interface should have two functions implemented
+
+// A class can have multiple interfaces
+public interface Connectable {
+
+
+    void turnoff();
+    void turnon();
+
+}
